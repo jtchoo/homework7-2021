28f9230620c82e518808569ee91b01fb2068b8a2
jtchoo <89865545+jtchoo@users.noreply.github.com>
2021-11-17 15:09:21 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -10,6 +10,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play()
+	document.querySelector("#volume").textContent = video.volume * 100 + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
